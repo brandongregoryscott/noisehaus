4b3456721e93fd2df240d0926871d8ab38ca8ba8
Brandon Scott <contact@brandonscott.me>
2026-03-05 20:13:14 -0500

    fix(web): select board file by id instead of running separate query (#5)

--- apps/web/src/hooks/use-get-board-file.ts
+++ apps/web/src/hooks/use-get-board-file.ts
@@ -1,42 +1,23 @@
-import type {
-    ApiError,
-    ApiErrorResponse,
-    ApiSuccessResponse,
-    BoardFile,
-} from "common";
-import { useQuery } from "@tanstack/react-query";
-import { GET_BOARD_FILE_ROUTE } from "common";
-import { isEmpty } from "lodash-es";
-import { get } from "@/utils/fetch";
-import { generateRoute } from "@/utils/route-utils";
+import type { PresignedBoardFile } from "common";
+import { useCallback } from "react";
+import { useListBoardFiles } from "@/hooks/use-list-board-files";
 
 type UseGetBoardFileOptions = {
     boardSlug: string;
     id: string;
+    token: string | undefined;
 };
 
 const useGetBoardFile = (options: UseGetBoardFileOptions) => {
-    const { boardSlug, id } = options;
-    const result = useQuery<BoardFile, ApiError>({
-        enabled: !isEmpty(boardSlug) && !isEmpty(id),
-        queryFn: async () => {
-            const route = generateRoute(GET_BOARD_FILE_ROUTE, {
-                id,
-                slug: boardSlug,
-            });
+    const { boardSlug, id, token } = options;
 
-            const { data, error } = await get<
-                ApiErrorResponse | ApiSuccessResponse<BoardFile>
-            >(route);
+    const selectById = useCallback(
+        (boardFiles: PresignedBoardFile[]) =>
+            boardFiles.find((boardFile) => boardFile.id === id),
+        [id]
+    );
 
-            if (error !== null) {
-                throw error;
-            }
-
-            return data;
-        },
-        queryKey: [GET_BOARD_FILE_ROUTE, boardSlug, id],
-    });
+    const result = useListBoardFiles({ boardSlug, select: selectById, token });
 
     return result;
 };
