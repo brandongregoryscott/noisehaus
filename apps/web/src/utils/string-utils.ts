422991b24680c3b9a2dd43140f2008a9e8ff8f15
Brandon Scott <contact@brandonscott.me>
2026-03-14 12:55:19 -0400

    refactor(web): vendorize data from faker for smaller bundle, more interesting names

--- apps/web/src/utils/string-utils.ts
+++ apps/web/src/utils/string-utils.ts
@@ -1,9 +1,12 @@
-import { faker } from "@faker-js/faker/locale/en";
 import { kebabCase } from "lodash-es";
+import { ADJECTIVES, ANIMAL_TYPES, COLORS } from "@/constants";
 
 const generateSlug = (): string =>
-    [faker.hacker.ingverb(), faker.color.human(), faker.animal.type()]
+    [randomItem(ADJECTIVES), randomItem(COLORS), randomItem(ANIMAL_TYPES)]
         .map(kebabCase)
         .join("-");
+
+const randomItem = <T>(items: T[]): T =>
+    items[Math.floor(Math.random() * items.length)];
 
 export { generateSlug };
