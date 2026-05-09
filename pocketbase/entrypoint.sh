ec4affc31610ff9da599c6f5b202fd443d81d495
Brandon Scott <contact@brandonscott.me>
2026-05-09 13:54:08 -0400

    fix: migrate pocketbase db, seed superuser (#27)

---
+++ pocketbase/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -e
+
+/usr/local/bin/pocketbase --dir=/pb_data --migrationsDir=/pb_migrations migrate up
+/usr/local/bin/pocketbase --dir=/pb_data --migrationsDir=/pb_migrations superuser upsert "$POCKETBASE_SUPERUSER_EMAIL" "$POCKETBASE_SUPERUSER_PASSWORD"
+
+exec /usr/local/bin/pocketbase serve --http=0.0.0.0:8090 --dir=/pb_data --migrationsDir=/pb_migrations
